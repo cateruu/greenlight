9bc59c596d495c9377955ee8fc3e97b7b4f5b05c
Paweł Kromołowski <cater981pl@gmail.com>
2024-02-06 16:08:16 +0100

    rate limiter

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -53,3 +53,7 @@ func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Requ
 	message := "unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
+
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	app.errorResponse(w, r, http.StatusTooManyRequests, "rate limit exceeded")
+}
